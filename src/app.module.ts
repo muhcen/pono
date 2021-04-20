5d4d973c4ff04a6fc23f0d436f916a81c93f19c1
muhcen <mohsenun7.@gmail.com>
2021-04-20 19:51:51 +0430

    create products file for product

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { typeOrmConfig } from './config/typeorm.config';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [AuthModule, TypeOrmModule.forRoot(typeOrmConfig)],
+  imports: [AuthModule, TypeOrmModule.forRoot(typeOrmConfig), ProductsModule],
 })
 export class AppModule {}

fcc0b4bb84288d71b3f800bf50f07921cc4534b9
muhcen <mohsenun7@gmail.com>
2021-04-27 20:37:53 +0430

    add config file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { typeOrmConfig } from './config/typeorm.config';
 import { ProductsModule } from './products/products.module';
+require('dotenv').config({ path: __dirname + '/../config.env' });
 
 @Module({
-    imports: [AuthModule, TypeOrmModule.forRoot(typeOrmConfig), ProductsModule],
+    imports: [
+        AuthModule,
+        TypeOrmModule.forRoot(typeOrmConfig),
+        MongooseModule.forRoot(process.env.DATA_BASE_URL_MONGODB),
+        ProductsModule,
+    ],
 })
 export class AppModule {}

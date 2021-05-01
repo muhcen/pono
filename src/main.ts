235fb3f36889b59e38098a720d0848307d2cf9a1
muhcen <mohsenun7@gmail.com>
2021-05-01 19:29:02 +0430

    add some security

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { LoggingInterceptor } from './products/interceptor/logging.interceptor';
 import * as cookieParser from 'cookie-parser';
+import * as helmet from 'helmet';
+import * as csurf from 'csurf';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.use(helmet());
+    app.use(csurf());
+    app.enableCors();
     app.use(cookieParser());
     app.useGlobalInterceptors(new LoggingInterceptor());
     app.useGlobalPipes(new ValidationPipe());
